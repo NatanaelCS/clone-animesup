c9d6f5aa3ee71368fcd4dd9e49afc3bfe4f8fdf8
Natan <natan.carvalho123Hotmail.com>
2021-02-13 14:07:31 -0300

    Finalizando a Home

--- src/database/database.ts
+++ src/database/database.ts
@@ -27,51 +27,84 @@ export default {
   ],
   recentlyAdded: [
     {
-      name: 'Hataraku Saibou Black',
-      url: 'https://image.tmdb.org/t/p/w185/53EsdweW8rCjMmFsPB2LnohCDy1.jpg'
-    },{
-      name: 'Dragon Quest',
-      url: 'https://image.tmdb.org/t/p/w185/yP8lEGxaL4KFtYVP5SskHcYVbz4.jpg'
+      name: 'Radiant Dublado',
+      url: 'https://image.tmdb.org/t/p/w300/8SVvTKIiI3S5vgVEH8CBxP9sFGM.jpg'
+    },
+    {
+      name: 'Hanyou no Yashahime: Sengoku Otogi',
+      url: 'https://animesup.net/wp-content/uploads/2021/02/hanyou-1x19-1-300x170.jpg'
+    },
+    {
+      name: 'Dragon Quest: Dai no Daibouken',
+      url: 'https://image.tmdb.org/t/p/w300/cfjq1AWGPhZ1KMiOSUom7qIpQt7.jpg'
     },
     {
       name: 'Pokemon (2019)',
-      url: 'https://animesup.net/wp-content/uploads/2020/06/3ONEUTJEvuU00gE1Pm0QmiPEvWv-1-185x278.jpg'
+      url: 'https://animesup.net/wp-content/uploads/2020/11/pokemon-55-300x170.jpg'
     },
     {
-      name: 'Project Sacrd',
-      url: 'https://image.tmdb.org/t/p/w185/qyMM0PVhHNocADOGex4j2uyBQpL.jpg'
+      name: 'Radiant Dublado',
+      url: 'https://image.tmdb.org/t/p/w300/8SVvTKIiI3S5vgVEH8CBxP9sFGM.jpg'
     },
     {
-      name: 'Hanyou no Yashahime',
-      url: 'https://image.tmdb.org/t/p/w185/tH11NADsVzax7HCg3Oy1gdCUzl1.jpg'
+      name: 'Hanyou no Yashahime: Sengoku Otogi',
+      url: 'https://animesup.net/wp-content/uploads/2021/02/hanyou-1x19-1-300x170.jpg'
     },
     {
-      name: 'Hataraku Saibou',
-      url: 'https://image.tmdb.org/t/p/w185/kSltzpIOKIjXGWgxc07rzikynvG.jpg'
+      name: 'Dragon Quest: Dai no Daibouken',
+      url: 'https://image.tmdb.org/t/p/w300/cfjq1AWGPhZ1KMiOSUom7qIpQt7.jpg'
     },
     {
-      name: 'Hataraku Saibou Black',
-      url: 'https://image.tmdb.org/t/p/w185/53EsdweW8rCjMmFsPB2LnohCDy1.jpg'
+      name: 'Pokemon (2019)',
+      url: 'https://animesup.net/wp-content/uploads/2020/11/pokemon-55-300x170.jpg'
     },
     {
-      name: 'Hataraku Saibou Black',
-      url: 'https://image.tmdb.org/t/p/w185/53EsdweW8rCjMmFsPB2LnohCDy1.jpg'
+      name: 'Radiant Dublado',
+      url: 'https://image.tmdb.org/t/p/w300/8SVvTKIiI3S5vgVEH8CBxP9sFGM.jpg'
     },
     {
-      name: 'Hataraku Saibou Black',
-      url: 'https://image.tmdb.org/t/p/w185/53EsdweW8rCjMmFsPB2LnohCDy1.jpg'
+      name: 'Hanyou no Yashahime: Sengoku Otogi',
+      url: 'https://animesup.net/wp-content/uploads/2021/02/hanyou-1x19-1-300x170.jpg'
     },
     {
-      name: 'Hataraku Saibou Black',
-      url: 'https://image.tmdb.org/t/p/w185/53EsdweW8rCjMmFsPB2LnohCDy1.jpg'
+      name: 'Dragon Quest: Dai no Daibouken',
+      url: 'https://image.tmdb.org/t/p/w300/cfjq1AWGPhZ1KMiOSUom7qIpQt7.jpg'
     },
     {
-      name: 'Project Sacrd',
-      url: 'https://image.tmdb.org/t/p/w185/qyMM0PVhHNocADOGex4j2uyBQpL.jpg'
+      name: 'Pokemon (2019)',
+      url: 'https://animesup.net/wp-content/uploads/2020/11/pokemon-55-300x170.jpg'
     },
     {
-      name: 'Hanyou no Yashahime',
-      url: 'https://image.tmdb.org/t/p/w185/tH11NADsVzax7HCg3Oy1gdCUzl1.jpg'
+      name: 'Radiant Dublado',
+      url: 'https://image.tmdb.org/t/p/w300/8SVvTKIiI3S5vgVEH8CBxP9sFGM.jpg'
     },
+    {
+      name: 'Hanyou no Yashahime: Sengoku Otogi',
+      url: 'https://animesup.net/wp-content/uploads/2021/02/hanyou-1x19-1-300x170.jpg'
+    },
+    {
+      name: 'Dragon Quest: Dai no Daibouken',
+      url: 'https://image.tmdb.org/t/p/w300/cfjq1AWGPhZ1KMiOSUom7qIpQt7.jpg'
+    },
+    {
+      name: 'Pokemon (2019)',
+      url: 'https://animesup.net/wp-content/uploads/2020/11/pokemon-55-300x170.jpg'
+    },
+    {
+      name: 'Radiant Dublado',
+      url: 'https://image.tmdb.org/t/p/w300/8SVvTKIiI3S5vgVEH8CBxP9sFGM.jpg'
+    },
+    {
+      name: 'Hanyou no Yashahime: Sengoku Otogi',
+      url: 'https://animesup.net/wp-content/uploads/2021/02/hanyou-1x19-1-300x170.jpg'
+    },
+    {
+      name: 'Dragon Quest: Dai no Daibouken',
+      url: 'https://image.tmdb.org/t/p/w300/cfjq1AWGPhZ1KMiOSUom7qIpQt7.jpg'
+    },
+    {
+      name: 'Pokemon (2019)',
+      url: 'https://animesup.net/wp-content/uploads/2020/11/pokemon-55-300x170.jpg'
+    }
   ]
 }
